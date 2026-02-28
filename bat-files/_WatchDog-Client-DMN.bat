1c7f22028b1b08e70a4c10ad147fb37dc8fcc5e1
tobiasconstantin <tobi.feldmeier@gmail.com>
2026-02-28 06:08:12 +0100

    watchdog dmn

---
+++ bat-files/_WatchDog-Client-DMN.bat
@@ -0,0 +1,4 @@
+timeout /t 5 /nobreak >nul
+@echo off
+cd "C:\Program Files\vvvv\vvvv_gamma_7.0-win-x64\"
+start vvvv.exe -o "C:\Users\SHA.ART\Documents\AltePost\AltePostWien-GAS\WatchDog-AltePost\WatchDog_Operator_Client-DMN.vl" --allowmultiple --package-repositories C:\Users\SHA.ART\Documents\vvvv\vl-libs --editable-packages VL.Fuse*
